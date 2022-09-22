9a0ad3589a5d298567ba89608536b6bab73796b8
Rokas <rookas.rudzenskas@gmail.com>
2022-09-22 13:18:21 +0300

    Rokas

--- app/hooks/todo.js
+++ app/hooks/todo.js
@@ -1,62 +1,13 @@
 import * as anchor from '@project-serum/anchor'
 import { useEffect, useMemo, useState } from 'react'
 import { TODO_PROGRAM_PUBKEY } from '../constants'
-import todoIDL from '../constants/idl.js'
+import { IDL as profileIdl } from '../constants/idl'
 import toast from 'react-hot-toast'
 import { SystemProgram } from '@solana/web3.js'
 import { utf8 } from '@project-serum/anchor/dist/cjs/utils/bytes'
 import { findProgramAddressSync } from '@project-serum/anchor/dist/cjs/utils/pubkey'
 import { useAnchorWallet, useConnection, useWallet } from '@solana/wallet-adapter-react'
 import { authorFilter } from '../utils'
-
-// Static data that reflects the todo struct of the solana program
-let dummyTodos = [
-    {
-        account:{
-            idx: '0',
-            content: 'Finish the essay collaboration',
-            marked: false,
-        }
-
-    },
-    {
-        account:{
-            idx: '1',
-            content: 'Understand Static Todo App',
-            marked: false,
-        }
-
-    },
-    {
-        account:{
-            idx: '2',
-            content: 'Read next chapter of the book in Danish',
-            marked: false,
-        }
-    },
-    {
-        account:{
-            idx: '3',
-            content: 'Do the math for next monday',
-            marked: false,
-        }
-    },
-    {
-        account:{
-            idx: '4',
-            content: 'Send the finished assignment',
-            marked: true,
-        }
-    },
-    {
-        account:{
-            idx: '5',
-            content: 'Read english book chapter 5',
-            marked: true,
-        }
-    },
-]
-
 
 export function useTodo() {
     const { connection } = useConnection()
@@ -68,56 +19,49 @@ export function useTodo() {
     const [todos, setTodos] = useState([])
     const [loading, setLoading] = useState(false)
     const [transactionPending, setTransactionPending] = useState(false)
-    const [input, setInput] = useState("")
-
 
     const program = useMemo(() => {
         if (anchorWallet) {
             const provider = new anchor.AnchorProvider(connection, anchorWallet, anchor.AnchorProvider.defaultOptions())
-            return new anchor.Program(todoIDL, TODO_PROGRAM_PUBKEY, provider)
+            return new anchor.Program(profileIdl, TODO_PROGRAM_PUBKEY, provider)
         }
     }, [connection, anchorWallet])
 
     useEffect(() => {
-            // fetch the userProfile if there is a profile the get it is TodoAccounts
         const findProfileAccounts = async () => {
             if (program && publicKey && !transactionPending) {
                 try {
-                    setLoading(true);
-                    const [profilePda, profileBump] = findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId);
-                    const profileAccount = await program.account.userProfile.fetch(profilePda);
+                    setLoading(true)
+                    const [profilePda, profileBump] = await findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
+                    const profileAccount = await program.account.userProfile.fetch(profilePda)
 
-                    if(profileAccount) {
-                        setLastTodo(profileAccount.lastTodo);
-                        setInitialized(true);
+                    if (profileAccount) {
+                        setLastTodo(profileAccount.lastTodo)
+                        setInitialized(true)
 
-                        const todoAccounts = await program.account.todoAccount.all([authorFilter(publicKey.toString())]);
-                        setTodos(todoAccounts);
+                        const todoAccounts = await program.account.todoAccount.all([authorFilter(publicKey.toString())])
+                        setTodos(todoAccounts)
                     } else {
-                        setInitialized(false);
+                        setInitialized(false)
                     }
-
-                } catch(err) {
-                    console.log(err);
-                    setInitialized(false);
-                    setTodos([]);
+                } catch (error) {
+                    console.log(error)
+                    setInitialized(false)
+                    setTodos([])
                 } finally {
-                    setLoading(false);
+                    setLoading(false)
                 }
             }
         }
-            if(initialized) {
-            setTodos(dummyTodos)
-        }
 
         findProfileAccounts()
-    }, [publicKey, program, transactionPending]);
+    }, [publicKey, program, transactionPending])
 
     const initializeUser = async () => {
         if (program && publicKey) {
             try {
                 setTransactionPending(true)
-                const [profilePda, profileBump] = findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId);
+                const [profilePda, profileBump] = findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
 
                 const tx = await program.methods
                     .initializeUser()
@@ -126,14 +70,14 @@ export function useTodo() {
                         authority: publicKey,
                         systemProgram: SystemProgram.programId,
                     })
-                    .rpc();
-                setInitialized(true);
-                toast.success('Successfully initialized user.');
+                    .rpc()
+                setInitialized(true)
+                toast.success('Successfully initialized user.')
             } catch (error) {
-                console.log(error);
-                toast.error(error.toString());
+                console.log(error)
+                toast.error(error.toString())
             } finally {
-                setTransactionPending(false);
+                setTransactionPending(false)
             }
         }
     }
@@ -218,70 +162,14 @@ export function useTodo() {
                 console.log(error)
                 toast.error(error.toString())
             } finally {
-                setLoading(false)
-                setTransactionPending(false)
+                setLoading(false);
+                setTransactionPending(false);
             }
         }
     }
-
-    const handleChange = (e)=> {
-        setInput(e.target.value)
-    }
-
-    const initializeStaticUser = () => {
-        setInitialized(true)
-    }
-
-    const addStaticTodo = (e) => {
-        e.preventDefault()
-        if(input) {
-            const newTodo = {
-                account:{
-                    idx: parseInt(todos[todos.length-1].account.idx) + 1,
-                    content: input,
-                    marked: false
-                }
-            }
-            setTodos([newTodo,...todos])
-            setInput("")
-        }
-    }
-
-    const markStaticTodo = (todoID) => {
-        setTodos(
-          todos.map(todo => {
-            console.log(todo.account, todoID, "YTAAAAA")
-            if (todo.account.idx === todoID) {
-                console.log("MATCHED")
-                return {
-                  account: {
-                    idx: todo.account.idx,
-                    content: todo.account.content,
-                    marked: !todo.account.marked
-                  }
-                }
-            }
-
-            return todo
-          }),
-        )
-    }
-
-    const removeStaticTodo = async (todoID) => {
-        setTodos(
-            todos.filter(todo => {
-              if (todo.account.idx === todoID) {
-                return
-              }
-
-              return todo
-            }),
-          )
-    }
-
 
     const incompleteTodos = useMemo(() => todos.filter((todo) => !todo.account.marked), [todos])
     const completedTodos = useMemo(() => todos.filter((todo) => todo.account.marked), [todos])
 
-    return { initialized, initializeStaticUser, loading, transactionPending, completedTodos, incompleteTodos, markStaticTodo, removeStaticTodo, addStaticTodo, input, setInput, handleChange }
+    return { initialized, initializeUser, loading, transactionPending, completedTodos, incompleteTodos, addTodo, markTodo, removeTodo }
 }
