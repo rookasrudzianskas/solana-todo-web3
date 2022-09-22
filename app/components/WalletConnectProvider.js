733201a0df9ea18ffd7a3b420ff4c2a977f58448
Rokas <rookas.rudzenskas@gmail.com>
2022-09-22 09:21:34 +0300

    Rokas

---
+++ app/components/WalletConnectProvider.js
@@ -0,0 +1,35 @@
+import { useMemo } from 'react'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+
+export const WalletConnectProvider = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
+
+    // Provided a custom RPC endpoint.
+    const endpoint = useMemo(() => {
+        if (network === WalletAdapterNetwork.Devnet) {
+            return 'https://withered-orbital-mountain.solana-devnet.discover.quiknode.pro/8ddf07c963cfb151ee5298fda08385e1e82c8ff1/'
+        }
+
+        return clusterApiUrl(network)
+    }, [network]);
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded.
+
+    const wallets = useMemo(() => (), input);
+
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
